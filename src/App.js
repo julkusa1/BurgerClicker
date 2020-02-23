634fa80f5c35057545b57a6c8cb70d0cb0538ea7
Sanna Julkunen <sanna.julkunen1@gmail.com>
2020-02-23 12:52:39 +0200

    Reduce

--- src/App.js
+++ src/App.js
@@ -51,7 +51,9 @@ claimCoupon(couponId) {
           <Route path='/coupons' render={props => (
             <Coupons clicks={this.state.clicks} claimCoupon={this.claimCoupon}/>
           )} />
-          <Route path='/profile' component={Profile} />
+          <Route path='/profile' render={props => (
+            <Profile coupons={this.state.coupons} />
+           ) } />
           <Menu claimableCoupons={5} />
         </div>
       </Router>
